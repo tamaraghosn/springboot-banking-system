89623e539ed8ffb4be7a83648e89c002a632340d
tamaraghosn <tamaraghosn01@gmail.com>
2025-09-17 19:24:34 +0300

    Initializing project

---
+++ src/main/java/com/tamara/BankingAppSpringBoot/BankingAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tamara.BankingAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamara/BankingAppSpringBoot/BankingAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.BankingAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
