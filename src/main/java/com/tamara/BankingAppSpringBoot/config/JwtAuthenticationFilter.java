cf3f9f4d7272948171e77184b3f61281b299f21b
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-08 14:00:31 +0300

    create jwtAuthEntryPoint, setting up jwtUtil

---
+++ src/main/java/com/tamara/BankingAppSpringBoot/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.tamara.BankingAppSpringBoot.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
