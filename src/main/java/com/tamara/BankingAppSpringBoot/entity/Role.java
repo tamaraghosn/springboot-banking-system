437e7742056e9d8c69af971ffd7fa1350c721265
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-12 14:09:57 +0300

    implement userdetails in user entity to enable spring security auth and added role enum for RBA and created customUserDetails service to load users from the db by email

---
+++ src/main/java/com/tamara/BankingAppSpringBoot/entity/Role.java
@@ -0,0 +1,6 @@
+package com.tamara.BankingAppSpringBoot.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/tamara/BankingAppSpringBoot/entity/User.java
+++ src/main/java/com/tamara/BankingAppSpringBoot/entity/User.java
@@ -4,9 +4,14 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,7 +20,7 @@ import java.time.LocalDateTime;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -33,12 +38,49 @@ public class User {
     private String email;
     private  String password; // we're saving encrypted passsword
 
+
     private  String phoneNumber;
     private String alternativeNumber;
     private  String status;
+    private Role role;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime modifiedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
